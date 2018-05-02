c391e50b015eb0890666564ee32d36da79f21ab4
wbdana <william.b.dana@gmail.com>
2018-05-02 16:37:06 -0400

    Update configs

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
     app.run()
