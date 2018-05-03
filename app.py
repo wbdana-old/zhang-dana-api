0a7315b7c99cd29a5d4a38e7017bcbc34c058411
wbdana <william.b.dana@gmail.com>
2018-05-03 13:38:49 -0400

    Add Rsvp POST route

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
@@ -19,6 +18,26 @@ def index():
 def all_rsvps():
     query_result = Rsvp.query.all()
     return jsonify(result=[item.serialize for item in query_result])
+
+@app.route('/rsvp', methods=['POST'])
+def add_rsvp():
+    rsvp_obj = request.get_json()
+    if rsvp_obj['rehearsal'] == "True":
+        rsvp_obj['rehearsal'] = True
+    else:
+        rsvp_obj['rehearsal'] = False
+    if rsvp_obj['wedding'] == "True":
+        rsvp_obj['wedding'] = True
+    else:
+        rsvp_obj['wedding'] = False
+    if rsvp_obj['brunch'] == "True":
+        rsvp_obj['brunch'] = True
+    else:
+        rsvp_obj['brunch'] = False
+    new_rsvp = Rsvp(rsvp_obj['name'], rsvp_obj['email'], rsvp_obj['rehearsal'], rsvp_obj['wedding'], rsvp_obj['brunch'])
+    db.session.add(new_rsvp)
+    db.session.commit()
+    return '', 204
 
 
 if __name__ == '__main__':
