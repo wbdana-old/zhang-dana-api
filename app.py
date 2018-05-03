bdef838e440af9eb9e8c605d5b77fb0d5256e48e
wbdana <william.b.dana@gmail.com>
2018-05-03 17:47:49 -0400

    Handle CORS; work on POST Rsvp route

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/rsvp": {"origins": "http://localhost:3000/"}})
 db = SQLAlchemy(app)
 
 from models import Rsvp
@@ -20,8 +23,13 @@ def all_rsvps():
     return jsonify(result=[item.serialize for item in query_result])
 
 @app.route('/rsvp', methods=['POST'])
+@cross_origin(origin='https://zhang-dana.herokuapp.com', headers=['Content-Type'])
 def add_rsvp():
     rsvp_obj = request.get_json()
+    if rsvp_obj['name'] == "":
+        return 'Name cannot be blank!', 400
+    if rsvp_obj['email'] == "":
+        return 'Email cannot be blank!', 400
     if rsvp_obj['rehearsal'] == "True":
         rsvp_obj['rehearsal'] = True
     else:
@@ -37,7 +45,7 @@ def add_rsvp():
     new_rsvp = Rsvp(rsvp_obj['name'], rsvp_obj['email'], rsvp_obj['rehearsal'], rsvp_obj['wedding'], rsvp_obj['brunch'])
     db.session.add(new_rsvp)
     db.session.commit()
-    return '', 204
+    return 'We can\'t wait to see you!', 200
 
 
 if __name__ == '__main__':
